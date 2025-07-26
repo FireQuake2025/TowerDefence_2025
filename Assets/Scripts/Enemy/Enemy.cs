015e705f5c9a7e22bf48c266893c9fad1b2bdf8b
FireQuake2025 <marcus.foreman01@gmail.com>
2025-07-26 13:51:35 -0500

    Setting up Enemy Script

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,35 +1,43 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyMovment : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     private NavMeshAgent navAgent;
     [SerializeField] private Transform endPoint;
-    private Animator animation;
+    private Animator animator;
     [SerializeField] private string anamationPram_IsWalking;
+    [SerializeField] private int damage;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         navAgent = GetComponent<NavMeshAgent>();
-        animation = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     private void Start()
     {
         navAgent.SetDestination(endPoint.position);
-        animation.SetBool(anamationPram_IsWalking, true);
+        animator.SetBool(anamationPram_IsWalking, true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!navAgent.pathPending && navAgent.remainingDistance <= navAgent.stoppingDistance)
+        if (!navAgent.pathPending && navAgent.remainingDistance <= navAgent.stoppingDistance)
         {
             if (!navAgent.hasPath || navAgent.pathStatus == NavMeshPathStatus.PathComplete)
             {
-                animation.SetBool(anamationPram_IsWalking, false);
+                ReachEnd();
             }
         }
-        
+
+    }
+
+    private void ReachEnd()
+    {
+        animator.SetBool(anamationPram_IsWalking, false);
+        GameManager.Instance.playerHeath.TakeDamage(damage);
+        Destroy(gameObject);
     }
 }
